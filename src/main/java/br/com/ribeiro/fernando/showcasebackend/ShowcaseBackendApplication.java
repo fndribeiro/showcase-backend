8326acc8dabf8893733cb814a871a06fb048156c
Fernando Ribeiro <fribeiro@ibm.com>
2023-06-13 23:42:22 -0300

    Init commit

---
+++ src/main/java/br/com/ribeiro/fernando/showcasebackend/ShowcaseBackendApplication.java
@@ -0,0 +1,13 @@
+package br.com.ribeiro.fernando.showcasebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShowcaseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShowcaseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/ribeiro/fernando/showcasebackend/ShowcaseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ribeiro.fernando.showcasebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowcaseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
